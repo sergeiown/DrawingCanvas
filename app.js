b89176e041776dad5c8915b563a7407434b26b63
Serhii Myshko <sergeiown@gmail.com>
2023-02-28 19:01:58 +0200

    add a file save method

--- app.js
+++ app.js
@@ -1,28 +1,25 @@
-import {
-  canvasWidth as width,
-  canvasHeight as height,
-  canvasColor as bgColor,
-} from "./js/config.js";
-import DrawingCanvas from "./js/dc/dc.js";
-import ToolBar from "./js/toolbar.js";
-import { startDrawingLine } from "./js/draw-line.js";
-import { startDrawingTrapezoid } from "./js/draw-trapezoid.js";
-import { startDrawingRectangle } from "./js/draw-rectangle.js";
-import { startDrawingCircle } from "./js/draw-circle.js";
-import { startDrawingPolygon } from "./js/draw-polygon.js";
+import { canvasWidth as width, canvasHeight as height, canvasColor as bgColor } from './js/config.js';
+import DrawingCanvas from './js/dc/dc.js';
+import ToolBar from './js/toolbar.js';
+import { startDrawingLine } from './js/draw-line.js';
+import { startDrawingTrapezoid } from './js/draw-trapezoid.js';
+import { startDrawingRectangle } from './js/draw-rectangle.js';
+import { startDrawingCircle } from './js/draw-circle.js';
+import { startDrawingPolygon } from './js/draw-polygon.js';
 
 const dc = new DrawingCanvas(width, height, bgColor);
 const dcOverlay = new DrawingCanvas(width, height);
 const toolBar = new ToolBar({
-  Clear: { handler: () => dc.clear() },
-  Line: { handler: startDrawingLine(dc, dcOverlay) },
-  Circle: { handler: startDrawingCircle(dc, dcOverlay) },
-  Rectangle: { handler: startDrawingRectangle(dc, dcOverlay) },
-  Trapezoid: { handler: startDrawingTrapezoid(dc, dcOverlay) },
-  Poligon: { handler: startDrawingPolygon(dc, dcOverlay) },
+    Clear: { handler: () => dc.clear() },
+    Line: { handler: startDrawingLine(dc, dcOverlay) },
+    Circle: { handler: startDrawingCircle(dc, dcOverlay) },
+    Rectangle: { handler: startDrawingRectangle(dc, dcOverlay) },
+    Trapezoid: { handler: startDrawingTrapezoid(dc, dcOverlay) },
+    Poligon: { handler: startDrawingPolygon(dc, dcOverlay) },
+    SaveImage: { handler: () => dc.saveAsPng() },
 });
 
-toolBar.appendTo("._container");
-dc.appendTo("._container");
-dcOverlay.appendTo("._container");
-dcOverlay.canvas.style.marginTop = -height + "px";
+toolBar.appendTo('._container');
+dc.appendTo('._container');
+dcOverlay.appendTo('._container');
+dcOverlay.canvas.style.marginTop = -height + 'px';
--- js/dc/dc.js
+++ js/dc/dc.js
@@ -1,110 +1,124 @@
 export default class DrawingCanvas {
-  constructor(width, height, bgColor) {
-    this.width = width;
-    this.height = height;
-    this.canvas = document.createElement("canvas");
-    this.canvas.width = width;
-    this.canvas.height = height;
-    this.ctx = this.canvas.getContext("2d");
+    constructor(width, height, bgColor) {
+        this.width = width;
+        this.height = height;
+        this.canvas = document.createElement('canvas');
+        this.canvas.width = width;
+        this.canvas.height = height;
+        this.ctx = this.canvas.getContext('2d');
 
-    if (bgColor) {
-      this.bgColor = bgColor;
-      this.canvas.style.background = bgColor;
-    }
-  }
-
-  appendTo(selector) {
-    const parent = document.querySelector(selector);
-
-    if (!parent) {
-      throw new ReferenceError("There is no element like that: " + selector);
+        if (bgColor) {
+            this.bgColor = bgColor;
+            this.canvas.style.background = bgColor;
+        }
     }
 
-    parent.appendChild(this.canvas);
-  }
+    appendTo(selector) {
+        const parent = document.querySelector(selector);
 
-  clear() {
-    this.ctx.clearRect(0, 0, this.width, this.height);
-  }
+        if (!parent) {
+            throw new ReferenceError('There is no element like that: ' + selector);
+        }
 
-  trapezoid({ x, y, bottomLength, topLength, height, thickness, color }) {
-    const x1 = x - bottomLength / 2;
-    const x2 = x1 + bottomLength;
-    const x3 = x + topLength / 2;
-    const x4 = x3 - topLength;
-    const y1 = y;
-    const y2 = y;
-    const y3 = y - height;
-    const y4 = y3;
-
-    this.ctx.beginPath();
-    this.ctx.moveTo(x1, y1);
-    this.ctx.lineTo(x2, y2);
-    this.ctx.lineTo(x3, y3);
-    this.ctx.lineTo(x4, y4);
-    this.ctx.closePath();
-
-    if (thickness) {
-      this.ctx.strokeStyle = color;
-      this.ctx.lineWidth = thickness;
-      this.ctx.stroke();
-    } else {
-      this.ctx.fillStyle = color;
-      this.ctx.fill();
-    }
-  }
-
-  circle({ x, y, radius, color, thickness }) {
-    this.ctx.beginPath();
-    this.ctx.arc(x, y, radius, 0, Math.PI * 2);
-
-    if (!thickness) {
-      this.ctx.fillStyle = color;
-      this.ctx.fill();
-    } else {
-      this.ctx.strokeStyle = color;
-      this.ctx.lineWidth = thickness;
-      this.ctx.stroke();
-    }
-  }
-
-  rectangle({ x, y, width, height, color, thickness, x1, y1, x2, y2 }) {
-    if (!width || !height) {
-      x = x || x1;
-      y = y || y1;
-      width = x2 - x;
-      height = y2 - y;
+        parent.appendChild(this.canvas);
     }
 
-    if (thickness) {
-      this.ctx.strokeStyle = color;
-      this.ctx.lineWidth = thickness;
-      this.ctx.strokeRect(x, y, width, height);
-    } else {
-      this.ctx.fillStyle = color;
-      this.ctx.fillRect(x, y, width, height);
-    }
-  }
-
-  polygon({ points, color }) {
-    this.ctx.fillStyle = color;
-    this.ctx.beginPath();
-
-    for (const { x, y } of points) {
-      this.ctx.lineTo(x, y);
+    clear() {
+        this.ctx.clearRect(0, 0, this.width, this.height);
     }
 
-    this.ctx.closePath();
-    this.ctx.fill();
-  }
+    saveAsPng() {
+        const link = document.createElement('a');
+        const fileName = new Date().toLocaleString('ukr').replace(/[^\d]/g, '-');
+        console.log(fileName);
 
-  line({ x1, y1, x2, y2, thickness, color }) {
-    this.ctx.lineWidth = thickness;
-    this.ctx.lineCap = "round";
-    this.ctx.strokeStyle = color;
-    this.ctx.beginPath();
-    this.ctx.moveTo(x1, y1);
-    this.ctx.lineTo(x2, y2);
-    this.ctx.stroke();
-  }
+        link.href = this.canvas.toDataURL('image/png');
+        link.download = `image-${fileName}.png`;
+
+        document.body.appendChild(link);
+        link.click();
+
+        document.body.removeChild(link);
+    }
+
+    trapezoid({ x, y, bottomLength, topLength, height, thickness, color }) {
+        const x1 = x - bottomLength / 2;
+        const x2 = x1 + bottomLength;
+        const x3 = x + topLength / 2;
+        const x4 = x3 - topLength;
+        const y1 = y;
+        const y2 = y;
+        const y3 = y - height;
+        const y4 = y3;
+
+        this.ctx.beginPath();
+        this.ctx.moveTo(x1, y1);
+        this.ctx.lineTo(x2, y2);
+        this.ctx.lineTo(x3, y3);
+        this.ctx.lineTo(x4, y4);
+        this.ctx.closePath();
+
+        if (thickness) {
+            this.ctx.strokeStyle = color;
+            this.ctx.lineWidth = thickness;
+            this.ctx.stroke();
+        } else {
+            this.ctx.fillStyle = color;
+            this.ctx.fill();
+        }
+    }
+
+    circle({ x, y, radius, color, thickness }) {
+        this.ctx.beginPath();
+        this.ctx.arc(x, y, radius, 0, Math.PI * 2);
+
+        if (!thickness) {
+            this.ctx.fillStyle = color;
+            this.ctx.fill();
+        } else {
+            this.ctx.strokeStyle = color;
+            this.ctx.lineWidth = thickness;
+            this.ctx.stroke();
+        }
+    }
+
+    rectangle({ x, y, width, height, color, thickness, x1, y1, x2, y2 }) {
+        if (!width || !height) {
+            x = x || x1;
+            y = y || y1;
+            width = x2 - x;
+            height = y2 - y;
+        }
+
+        if (thickness) {
+            this.ctx.strokeStyle = color;
+            this.ctx.lineWidth = thickness;
+            this.ctx.strokeRect(x, y, width, height);
+        } else {
+            this.ctx.fillStyle = color;
+            this.ctx.fillRect(x, y, width, height);
+        }
+    }
+
+    polygon({ points, color }) {
+        this.ctx.fillStyle = color;
+        this.ctx.beginPath();
+
+        for (const { x, y } of points) {
+            this.ctx.lineTo(x, y);
+        }
+
+        this.ctx.closePath();
+        this.ctx.fill();
+    }
+
+    line({ x1, y1, x2, y2, thickness, color }) {
+        this.ctx.lineWidth = thickness;
+        this.ctx.lineCap = 'round';
+        this.ctx.strokeStyle = color;
+        this.ctx.beginPath();
+        this.ctx.moveTo(x1, y1);
+        this.ctx.lineTo(x2, y2);
+        this.ctx.stroke();
+    }
 }
