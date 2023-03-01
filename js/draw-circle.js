8d5411b8fbd92b9f102754aad39cba08322a1c87
Serhii Myshko <sergeiown@gmail.com>
2023-03-01 12:14:55 +0200

    second attempt

--- js/draw-circle.js
+++ js/draw-circle.js
@@ -44,40 +44,37 @@ export function startDrawingCircle(dc, dcOverlay) {
 export function startDrawingCircleTouch(dc, dcOverlay) {
     const { canvas } = dcOverlay;
 
-    return () => {
-        canvas.onmousedown = (e1) => {
-            window.ontouchmove = (e2) => {
-                const dx = e2.offsetX - e1.offsetX;
-                const dy = e2.offsetY - e1.offsetY;
-                const radius = Math.sqrt(dx ** 2 + dy ** 2);
-                dcOverlay.clear();
+    canvas.on('mousedown', (e1) => {
+        $(window).on('mousemove', (e2) => {
+            const dx = e2.offsetX - e1.offsetX;
+            const dy = e2.offsetY - e1.offsetY;
+            const radius = Math.sqrt(dx ** 2 + dy ** 2);
+            dcOverlay.clear();
 
-                dcOverlay.circle({
-                    x: e1.offsetX,
-                    y: e1.offsetY,
-                    radius,
-                    color: 'grey',
-                    thickness: 1,
-                });
-            };
+            dcOverlay.circle({
+                x: e1.offsetX,
+                y: e1.offsetY,
+                radius,
+                color: 'grey',
+                thickness: 1,
+            });
+        });
 
-            window.onmouseup = (e2) => {
-                window.ontouchmove = null;
-                window.onmouseup = null;
+        $(window).on('mouseup', (e2) => {
+            $(window).off('mousemove mouseup');
 
-                const dx = e2.offsetX - e1.offsetX;
-                const dy = e2.offsetY - e1.offsetY;
-                const radius = Math.sqrt(dx ** 2 + dy ** 2);
+            const dx = e2.offsetX - e1.offsetX;
+            const dy = e2.offsetY - e1.offsetY;
+            const radius = Math.sqrt(dx ** 2 + dy ** 2);
 
-                dcOverlay.clear();
+            dcOverlay.clear();
 
-                dc.circle({
-                    x: e1.offsetX,
-                    y: e1.offsetY,
-                    radius,
-                    color: getColors(),
-                });
-            };
-        };
-    };
+            dc.circle({
+                x: e1.offsetX,
+                y: e1.offsetY,
+                radius,
+                color: getColors(),
+            });
+        });
+    });
 }
