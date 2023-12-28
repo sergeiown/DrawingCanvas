58a20bb6878324e844ed32ebc487bd68f8a32823
Serhii Myshko <sergeiown@gmail.com>
2023-12-28 19:51:37 +0200

    add the ability to draw curves on the touchscreen

--- js/draw-curve.js
+++ js/draw-curve.js
@@ -1,14 +1,10 @@
 import { getColors } from './color-picker.js';
 import { getThickness } from './color-picker.js';
-export { startDrawingCurve };
+export { startDrawingCurve, startDrawingCurveTouch };
 
 function startDrawingCurve(dc, dcOverlay) {
     const { canvas } = dcOverlay;
     let isDrawing = false;
-
-    if (!dc.history) {
-        dc.history = [];
-    }
 
     return () => {
         canvas.onmousedown = (e1) => {
@@ -36,6 +32,42 @@ function startDrawingCurve(dc, dcOverlay) {
                 isDrawing = false;
                 window.onmousemove = null;
                 window.onmouseup = null;
+                dc.ctx.beginPath();
+            };
+        };
+    };
+}
+
+function startDrawingCurveTouch(dc, dcOverlay) {
+    const { canvas } = dcOverlay;
+    let isDrawing = false;
+
+    return () => {
+        canvas.ontouchstart = (e1) => {
+            const canvasRect = canvas.getBoundingClientRect();
+            isDrawing = true;
+            dc.ctx.beginPath();
+            dc.curve({
+                x1: e1.touches[0].clientX - canvasRect.left,
+                y1: e1.touches[0].clientY - canvasRect.top,
+                thickness: getThickness(),
+                color: 'grey',
+            });
+
+            window.ontouchmove = (e2) => {
+                if (!isDrawing) return;
+                dc.curve({
+                    x1: e2.touches[0].clientX - canvasRect.left,
+                    y1: e2.touches[0].clientY - canvasRect.top,
+                    thickness: getThickness(),
+                    color: getColors(),
+                });
+            };
+
+            window.ontouchend = () => {
+                isDrawing = false;
+                window.ontouchmove = null;
+                window.ontouchend = null;
                 dc.ctx.beginPath();
             };
         };
